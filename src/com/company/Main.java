fdacf8fdec8624b54dc0b413fc3e18ff1498fa39
robson <robson.flavio20@outlook.com>
2021-08-24 17:01:53 -0300

    Initial commit :)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,18 +9,19 @@ public class Main {
         Veiculo veiculo;
         ServicoVeiculo servicoVeiulo = new ServicoVeiculo();
 
-        String placa= JOptionPane.showInputDialog("Informe a placa do veiculo: ");
+        String placa= JOptionPane.showInputDialog("Informe a placa do veiculo: ").toUpperCase();
 
         while (!placa.equalsIgnoreCase("fim")){
             int ano= Integer.parseInt(JOptionPane.showInputDialog("Informe o ano do veiculo [ex: 0000] ou (FIM) sair: "));
             String motor= JOptionPane.showInputDialog("Informe o motor do veiculo: ");
             String chassi= JOptionPane.showInputDialog("Informe o chassi do veiculo: ");
-            String cor= JOptionPane.showInputDialog("Informe a cor do veiculo: ");
-            String montadora= JOptionPane.showInputDialog("Informe a montadora do veiculo: ");
+            String cor= JOptionPane.showInputDialog("Informe a cor do veiculo: ").toUpperCase();
+            String montadora= JOptionPane.showInputDialog("Informe a montadora do veiculo: ").toUpperCase();
             veiculo = new Veiculo(placa,ano,motor,chassi,cor,montadora);
 
             servicoVeiulo.retornarListaVeiculos(veiculo);
-            placa= JOptionPane.showInputDialog("Informe a placa do veiculo: ");
+            servicoVeiulo.validadorPlaca();
+            placa= JOptionPane.showInputDialog("Informe a placa do veiculo: ").toUpperCase();
         }
         System.out.println(servicoVeiulo.toString());
 
--- src/com/company/ServicoVeiculo.java
+++ src/com/company/ServicoVeiculo.java
@@ -14,16 +14,11 @@ public class ServicoVeiculo {
         this.listaVeiculos.add(veiculo);
     }
 
-    public int qtdeVeiculos() {
-        int qtd = this.listaVeiculos.size();
-        return qtd;
-    }
-
     public int idadesDosVeiculos(int idadeVeiculo) {
         int qtd = 0;
 
-        for(int i = 0; i < this.listaVeiculos.size(); ++i) {
-            if (2021 - ((Veiculo)this.listaVeiculos.get(i)).getAno() >= idadeVeiculo) {
+        for (Veiculo listaVeiculo : this.listaVeiculos) {
+            if (2021 - listaVeiculo.getAno() >= idadeVeiculo) {
                 ++qtd;
             }
         }
@@ -34,21 +29,20 @@ public class ServicoVeiculo {
     public float percentualCarros(int ano) {
         int qtd = 0;
 
-        for(int i = 0; i < this.listaVeiculos.size(); ++i) {
-            if (2021 - ((Veiculo)this.listaVeiculos.get(i)).getAno() <= ano) {
+        for (Veiculo listaVeiculo : this.listaVeiculos) {
+            if (2021 - listaVeiculo.getAno() <= ano) {
                 ++qtd;
             }
         }
 
-        float percentual = (float)qtd / Float.parseFloat(String.valueOf(this.listaVeiculos.size())) * 100.0F;
-        return percentual;
+        return (float)qtd / Float.parseFloat(String.valueOf(this.listaVeiculos.size())) * 100.0F;
     }
 
     public int qtdPorCorEMontadora(String cor, String montadora) {
         int qtd = 0;
 
-        for(int i = 0; i < this.listaVeiculos.size(); ++i) {
-            if (((Veiculo)this.listaVeiculos.get(i)).getCor().equalsIgnoreCase(cor) && ((Veiculo)this.listaVeiculos.get(i)).getMontadora().equalsIgnoreCase(montadora)) {
+        for (Veiculo listaVeiculo : this.listaVeiculos) {
+            if (listaVeiculo.getCor().equalsIgnoreCase(cor) && listaVeiculo.getMontadora().equalsIgnoreCase(montadora)) {
                 ++qtd;
             }
         }
@@ -58,19 +52,18 @@ public class ServicoVeiculo {
 
     public float percentualPorCorEMontadora(String cor, String montadora, String motor, int ano) {
         int qtd = 0;
-        for(int i = 0; i < this.listaVeiculos.size(); ++i) {
-            if (this.listaVeiculos.get(i).getCor().equalsIgnoreCase(cor) &&
-                this.listaVeiculos.get(i).getMontadora().equalsIgnoreCase(montadora) &&
-                this.listaVeiculos.get(i).getMotor().equalsIgnoreCase(motor) &&
-                this.listaVeiculos.get(i).getAno() > ano) {
+        for (Veiculo listaVeiculo : this.listaVeiculos) {
+            if (listaVeiculo.getCor().equalsIgnoreCase(cor) &&
+                    listaVeiculo.getMontadora().equalsIgnoreCase(montadora) &&
+                    listaVeiculo.getMotor().equalsIgnoreCase(motor) &&
+                    listaVeiculo.getAno() > ano) {
                 ++qtd;
             }
         }
 
-        float percentual = (float)(qtd / this.listaVeiculos.size() * 100);
-        return percentual;
+        return (float)(qtd / this.listaVeiculos.size() * 100);
     }
-    private ArrayList<Veiculo> listaDeVeiculos = new ArrayList();
+    private final ArrayList<Veiculo> listaDeVeiculos = new ArrayList();
 
     public void retornarListaDeVeiculos(Veiculo veiculo) {
         this.listaDeVeiculos.add(veiculo);
@@ -78,19 +71,19 @@ public class ServicoVeiculo {
     Veiculo veiculoNovo;
 
     public void validadorPlaca() {
-        String[] vogais = new String[]{"a", "e", "i", "o", "u"};
+        String[] vogais = new String[]{"A", "E", "I", "O", "U"};
 
-        for(int i = 0; i < this.listaVeiculos.size(); ++i) {
-            String placa = this.listaVeiculos.get(i).getPlaca();
-            int ano = this.listaVeiculos.get(i).getAno();
-            String motor = this.listaVeiculos.get(i).getMotor();
-            String chassi = this.listaVeiculos.get(i).getChasi();
-            String cor = this.listaVeiculos.get(i).getCor();
-            String montadora = this.listaVeiculos.get(i).getMontadora();
+        for (Veiculo listaVeiculo : this.listaVeiculos) {
+            String placa = listaVeiculo.getPlaca();
+            int ano = listaVeiculo.getAno();
+            String motor = listaVeiculo.getMotor();
+            String chassi = listaVeiculo.getChasi();
+            String cor = listaVeiculo.getCor();
+            String montadora = listaVeiculo.getMontadora();
             this.veiculoNovo = new Veiculo(placa, ano, motor, chassi, cor, montadora);
 
-            for(int j = 0; j < vogais.length; ++j) {
-                if (this.listaVeiculos.get(i).getPlaca().startsWith(vogais[j])) {
+            for (String vogai : vogais) {
+                if (listaVeiculo.getPlaca().startsWith(vogai)) {
                     int alterPlaca = Integer.parseInt(String.valueOf(placa.charAt(7)));
                     if (alterPlaca % 2 == 0) {
                         this.retornarListaDeVeiculos(this.veiculoNovo);
@@ -103,28 +96,33 @@ public class ServicoVeiculo {
     }
 
     public String dadosPlacaFiltrada(){
-        String veiculo="sem dados...";
-        for (int i = 0; i < listaDeVeiculos.size(); i++) {
-            veiculo="\nPlaca: "+ this.listaDeVeiculos.get(i).getPlaca()+
-                    "\nAno: "+ this.listaDeVeiculos.get(i).getAno()+
-                    "\nMotor: "+ this.listaDeVeiculos.get(i).getMotor()+
-                    "\nChassi :"+ this.listaDeVeiculos.get(i).getChasi()+
-                    "\nCor: "+ this.listaDeVeiculos.get(i).getPlaca()+
-                    "\nMontadora: "+ this.listaDeVeiculos.get(i).getPlaca();
+        ArrayList<String> veiculo = new ArrayList<>();
+
+        String dadosVeiculo;
+        for (Veiculo listaDeVeiculo : listaDeVeiculos) {
+            dadosVeiculo = "Placa: " + listaDeVeiculo.getPlaca() +
+                    "\nAno: " + listaDeVeiculo.getAno() +
+                    "\nMotor: " + listaDeVeiculo.getMotor() +
+                    "\nChassi :" + listaDeVeiculo.getChasi() +
+                    "\nCor: " + listaDeVeiculo.getCor() +
+                    "\nMontadora: " + listaDeVeiculo.getMontadora();
+
+            veiculo.add(dadosVeiculo);
 
         }
 
-        return veiculo;
+        return String.valueOf(veiculo);
     }
     @Override
     public String toString(){
-        return "\nA quantidade de veiculos na cidade é: "+ qtdeVeiculos()+
+        return  "\nA quantidade de veiculos na cidade é: "+ this.listaVeiculos.size()+
                 "\nA quantidade de veiculos com mais de 10 anos de circulação é: "+ idadesDosVeiculos(10)+
                 "\nA quantidade de veiculos com mais de 20 anos de circulação é: "+ idadesDosVeiculos(20)+
                 "\nA quantidade de veiculos com menos de 5 anos de circulação é: "+ percentualCarros(5)+
                 "\nA quantidade de veiculos na cor BRANCA da montadora FIAT é: "+ qtdPorCorEMontadora("branca", "fiat")+
-                "\nA quantidade de veiculos na cor PRATA da montadora RENAULT, com motor 1.0 e ano superior a 1995: "+ percentualPorCorEMontadora("prata","renault","1.0",1995)+
-                "\nA Lista de veiculos que começam com vogais e terminam com numero par é: "+dadosPlacaFiltrada();
+                "\nA quantidade de veiculos na cor PRATA da montadora RENAULT, com motor 1.0 e ano superior a 1995: "+
+                percentualPorCorEMontadora("prata","renault","1.0",1995)+"\n"+
+                "\nA Lista de veiculos que começam com vogais e terminam com numero par é: \n\n"+dadosPlacaFiltrada();
 
     }
 }
